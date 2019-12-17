b768a06751d37eff908a99437aaa73ccfdd0671c
xxl <842224751@qq.com>
2019-12-17 19:10:12 +0800

    the first one

---
+++ git/src/main/java/com/xxl/git/GitApplication.java
@@ -0,0 +1,14 @@
+package com.xxl.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitApplication.class, args);
+        System.out.println("hahah");
+    }
+
+}
---
+++ git/src/test/java/com/xxl/git/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxl.git;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
